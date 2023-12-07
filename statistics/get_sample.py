2ff53270c93ce716d8818ff5bf2e25fb92c33b7a
Nora Abdelmageed <nora.youssef90@gmail.com>
2023-12-07 10:51:43 +0100

    bug fix: ensure unique randomization of tables

--- statistics/get_sample.py
+++ statistics/get_sample.py
@@ -13,7 +13,7 @@ seed(42)
 Results_Path = join(realpath('..'), 'data', 'results')
 Data_Path = join(realpath('..'), 'data', 'results')
 
-dataset_names = ['tfood', 'tfood10']
+dataset_names = ['tfood10']
 
 sample_size = 3  # this is percent value --> 1%
 
@@ -25,17 +25,24 @@ chunksize = 10000  # How many lines to be processed at the same time - lazy load
 
 def get_random_table_names(tables_path, table_type, dest_path):
     tables = listdir(tables_path)
-    print(f'Horizontal Tables: {len(tables)}')
+    print(f'{table_type} Tables: {len(tables)}')
 
     # calculate sample size
     dataset_sample_size = ceil((sample_size / 100) * len(tables))
     print(f'Generating Sample {table_type} Tables: {dataset_sample_size}')
 
-    rand_indx = [randint(0, dataset_sample_size) for _ in range(dataset_sample_size)]
+    # rand_indx = [randint(0, dataset_sample_size) for _ in range(dataset_sample_size)]
+    rand_indx = []
+    for _ in range(len(tables) * 1000):
+        new_rand = randint(0, len(tables))
+        if new_rand not in rand_indx:
+            rand_indx.append(new_rand)
+        if len(rand_indx) == dataset_sample_size:
+            break
 
     sample_tables = [tables[i] for i in rand_indx]
 
-    [shutil.copy2(src=join(tables_path, st), dst=dest_path) for st in sample_tables]
+    [shutil.copy(src=join(tables_path, st), dst=dest_path) for st in sample_tables]
 
     sample_tables = [splitext(t)[0] for t in sample_tables]
     return sample_tables
@@ -48,20 +55,28 @@ def sample_targets_gt(sample_tables, targets_path, dest_targets_path):
     targets = listdir(targets_path)
 
     for target in targets:
+        cnt = 0
+        total_lines = 0
+        unique_entries = []
         # Lazy loading with pandas to handle huge files e.g., cea_targets and gt data.
-        with pd.read_csv(join(targets_path, target), chunksize=chunksize, header=0) as reader:
+        with pd.read_csv(join(targets_path, target), chunksize=chunksize, header=None) as reader:
             for df in reader:
+
                 # Add header to the dataframe to enable a vectorized code for filteration
                 cols = ['file'] + [f'col{i}' for i in range(df.shape[1] - 1)]
                 df = df.set_axis(cols, axis=1)
 
+                total_lines += len(df)
+                unique_entries.extend(df['file'])
+                unique_entries = list(set(unique_entries))
+
                 # actual filteration step
                 df = df[df['file'].isin(sample_tables)]
 
-                print(f'Appending Lines: {len(df)}  to: {target}')
-
                 # save it to the destination targets with append mode
-                df.to_csv(join(dest_targets_path, target), mode='a', header=0, index=0)
+                df.to_csv(join(dest_targets_path, target), mode='a', header=None, index=None)
+                cnt += len(df)
+        print(f'Appending Lines: {cnt} / {total_lines} to: {target} - Unique entries: {len(unique_entries)}')
     print('=============================================')
 
 
